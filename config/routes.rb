396747827541f01ac204b63c617eda5affbe8c26
unknown <maryam.muhammed@gmail.com>
2020-10-19 10:08:54 +0100

    user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
+
+  get 'users/login'
+
+  post 'users/signin', to: 'users#signin'
+
+  get 'users/dashboard'
+  get 'user/logout'
+  get 'user/set_user'
+  post 'user/update'
+
   resources :admin
   resources :projects
   resources :users
-
+  
   root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
