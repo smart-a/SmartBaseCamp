f9bc0a5f504561a07a229a2c074e47bfa9f49899
smart-a <olalekan.adesoye@gmail.com>
2020-10-27 14:11:47 +0100

    merged with user_branch

--- app/models/project_user.rb
+++ app/models/project_user.rb
@@ -1,5 +1,6 @@
 class ProjectUser < ApplicationRecord
     belongs_to :project
+    has_many :users
     validates_uniqueness_of(:user_id, scope: :project_id)
     
 end
---
+++ db/migrate/20201027130155_redo_project_user.rb
@@ -0,0 +1,10 @@
+class RedoProjectUser < ActiveRecord::Migration[6.0]
+  def change
+    drop_table :project_users
+    create_table :project_users do |t|
+      t.integer :project_id
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
